18808850c200a35d4dba88bba423be75a0c76bf8
yangliu <342585705@qq.com>
2018-07-27 17:55:11 +0800

    add netty

--- netty/src/main/java/com/willow/NettyClient.java
+++ netty/src/main/java/com/willow/NettyClient.java
@@ -1,49 +1,59 @@
 package com.willow;
-import org.jboss.netty.bootstrap.ServerBootstrap;
+
+import org.jboss.netty.bootstrap.ClientBootstrap;
 import org.jboss.netty.channel.*;
-import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
+import org.jboss.netty.channel.socket.nio.NioClientSocketChannelFactory;
 import org.jboss.netty.handler.codec.string.StringDecoder;
 import org.jboss.netty.handler.codec.string.StringEncoder;
 
 import java.net.InetSocketAddress;
+import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 /**
  * @author Administrator
- * @create 2018-07-27 14:32
+ * @create 2018-07-27 17:30
  */
-public class NettyServer {
+public class NettyClient {
 
     public static void main(String arg[]){
         //1、创建服务器对象
-        ServerBootstrap serverBootstrap=new ServerBootstrap();
+        ClientBootstrap clientBootstrap=new ClientBootstrap();
         //2、创建两个线程池 ，第一个监听端口，nio监听
         ExecutorService boos = Executors.newCachedThreadPool();
         ExecutorService wook = Executors.newCachedThreadPool();
-        //3、把两个线程池加入中
-        serverBootstrap.setFactory(new NioServerSocketChannelFactory(boos,wook));
+        //3、把两个线程池加入中,创建NioClientSocketChannelFactory
+        clientBootstrap.setFactory(new NioClientSocketChannelFactory(boos,wook));
         //4、设置管道工厂
-        serverBootstrap.setPipelineFactory(new ChannelPipelineFactory() {
+        clientBootstrap.setPipelineFactory(new ChannelPipelineFactory() {
             //设置管道
             @Override
             public ChannelPipeline getPipeline() throws Exception {
                 ChannelPipeline channelPipeline=Channels.pipeline();
                 channelPipeline.addLast("decoder",new StringDecoder());
                 channelPipeline.addLast("encoder",new StringEncoder());
-                channelPipeline.addLast("serverHandler",new ServerHanlder());
+                channelPipeline.addLast("ClientHandler",new ServerHanlder());
                 //设置事件监听类
                 return channelPipeline;
             }
         });
 
-        serverBootstrap.bind(new InetSocketAddress(8080));
-        System.out.println("netty 服务器端启动完毕。。。。。。。。。。");
+        //绑定端口
+        ChannelFuture connect = clientBootstrap.connect(new InetSocketAddress("127.0.0.1", 8080));
+        System.out.println("netty 客户端端启动完毕。。。。。。。。。。");
+        Channel channel = connect.getChannel();
+        Scanner scanner=new Scanner(System.in);
+        while(true){
+            System.out.println("请输入内容：");
+            channel.write(scanner.next());
+        }
 
-        //访问http://localhost:8080/  查看控制台打印效果
+
     }
 }
-class ServerHanlder extends SimpleChannelHandler{
+
+class ClientHanlder extends SimpleChannelHandler{
 
     /**
      * 通道关闭的时候触发
@@ -79,6 +89,8 @@ class ServerHanlder extends SimpleChannelHandler{
     public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) throws Exception {
         super.messageReceived(ctx, e);
         System.out.println("messageReceived................");
-        System.out.println("threadName:"+Thread.currentThread().getName()+" 服务器接受的消息是: \n"+e.getMessage());
+        System.out.println("threadName:"+Thread.currentThread().getName()+" 客户端接受的消息是: \n"+e.getMessage());
+        ctx.getChannel().write("hello");//返回
     }
 }
+
